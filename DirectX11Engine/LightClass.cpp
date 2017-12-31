780e2401d83327b9fa2ef3cb3da05a8b9666a82e
Gregory Weiner <gregory.weiner@gmail.com>
2017-12-31 12:13:44 +0800

    [INCREMENTAL] Encapsulating light info into VS/PS structs

--- DirectX11Engine/LightClass.cpp
+++ DirectX11Engine/LightClass.cpp
@@ -20,26 +20,48 @@ void LightClass::SetAmbientColor(const XMFLOAT4& rgba/*float red, float green, f
 	_ambientColor = rgba;//XMFLOAT4(red, green, blue, alpha);
 }
 
-void LightClass::SetDiffuseColor(const XMFLOAT4& rgba/*float red, float green, float blue, float alpha*/)
+//~~~~ Info stored in VS Buffers ~~~~
+//void LightClass::SetPosition(const XMFLOAT3& pos/*float x, float y, float z*/)
+//{
+//	_position = pos;
+//}
+
+void LightClass::GenerateViewMatrix()
 {
-	_diffuseColor = rgba;// XMFLOAT4(red, green, blue, alpha);
+	_viewMatrix = DirectX::XMMatrixLookAtLH(XMLoadFloat3(&_position), XMLoadFloat3(&_lookAt), XMLoadFloat3(&XMFLOAT3(0,1,0)));
 }
 
-void LightClass::SetPosition(const XMFLOAT3& pos/*float x, float y, float z*/)
+void LightClass::GenerateProjectionMatrix(float screenDepth, float screenNear)
 {
-	//_position = XMFLOAT4(x, y, z, 1.f);
-	_position = pos;// XMFLOAT3(x, y, z);
+	float fieldOfView = (float)XM_PI / 2.0f;
+	float screenAspect = 1.0f;
+	_projectionMatrix = XMMatrixPerspectiveFovLH(fieldOfView, screenAspect, screenNear, screenDepth);
 }
 
+XMMATRIX LightClass::GetViewMatrix() const
+{
+	return _viewMatrix;
+}
+
+XMMATRIX LightClass::GetProjectionMatrix() const
+{
+	return _projectionMatrix;
+}
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~ Info stored in PS Buffers ~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 void LightClass::SetDirection(const XMFLOAT3& dir/*float x, float y, float z*/)
 {
-	_direction = dir;// XMFLOAT3(x, y, z);
+	_direction = dir;
 }
 
 void LightClass::SetSpecularColor(const XMFLOAT4& rgba/*float red, float green, float blue, float alpha*/)
 {
-	_specularColor = rgba;// XMFLOAT4(red, green, blue, alpha);
+	_specularColor = rgba;
 }
 
 void LightClass::SetSpecularPower(float power)
@@ -47,12 +69,15 @@ void LightClass::SetSpecularPower(float power)
 	_specularPower = power;
 }
 
+void LightClass::SetDiffuseColor(const XMFLOAT4& rgba/*float red, float green, float blue, float alpha*/)
+{
+	_diffuseColor = rgba;// XMFLOAT4(red, green, blue, alpha);
+}
+
+//~~~ To encapsulate later!!!!
 void LightClass::SetLookAt(const XMFLOAT3& lookAt/*float x, float y, float z*/)
 {
 	_lookAt = lookAt;
-	//_lookAt.x = x;
-	//_lookAt.y = y;
-	//_lookAt.z = z;
 }
 
 XMFLOAT4 LightClass::GetAmbientColor() const 
@@ -64,11 +89,6 @@ XMFLOAT4 LightClass::GetDiffuseColor() const
 {
 	return _diffuseColor;
 }
-
-//XMFLOAT4 LightClass::GetPosition() const
-//{
-//	return _position;
-//}
 
 XMFLOAT3 LightClass::GetPosition() const
 {
@@ -88,39 +108,4 @@ XMFLOAT4 LightClass::GetSpecularColor() const
 float LightClass::GetSpecularPower() const
 {
 	return _specularPower;
-}
-
-void LightClass::GenerateViewMatrix()
-{
-	XMFLOAT3 up;
-
-	// Setup the vector that points upwards.
-	up.x = 0.0f;
-	up.y = 1.0f;
-	up.z = 0.0f;
-
-	// Create the view matrix from the three vectors.
-	//XMFLOAT3 pos(_position.x, _position.y, _position.z);
-	//_viewMatrix = XMMatrixLookAtLH(XMLoadFloat3(&pos), XMLoadFloat3(&_lookAt), XMLoadFloat3(&up));
-	_viewMatrix = DirectX::XMMatrixLookAtLH(XMLoadFloat3(&_position), XMLoadFloat3(&_lookAt), XMLoadFloat3(&up));
-
-}
-
-void LightClass::GenerateProjectionMatrix(float screenDepth, float screenNear)
-{
-	float fieldOfView = (float)XM_PI / 2.0f;
-	float screenAspect = 1.0f;
-
-	// Create the projection matrix for the light.
-	_projectionMatrix = XMMatrixPerspectiveFovLH(fieldOfView, screenAspect, screenNear, screenDepth);
-}
-
-XMMATRIX LightClass::GetViewMatrix() const
-{
-	return _viewMatrix;
-}
-
-XMMATRIX LightClass::GetProjectionMatrix() const
-{
-	return _projectionMatrix;
 }
--- DirectX11Engine/LightClass.h
+++ DirectX11Engine/LightClass.h
@@ -20,27 +20,30 @@ public:
 	LightClass(const LightClass&);
 	~LightClass();
 
-	void SetAmbientColor(const XMFLOAT4&/*float, float, float, float*/);
-	void SetDiffuseColor(const XMFLOAT4&/*float, float, float, float*/);
-	void SetPosition(const XMFLOAT3&/*float x, float y, float z*/);
-	void SetDirection(const XMFLOAT3&/*float, float, float*/);
-	void SetSpecularColor(const XMFLOAT4&/*float, float, float, float*/);
-	void SetSpecularPower(float);
-	void SetLookAt(const XMFLOAT3&/*float, float, float*/);
-
-	XMFLOAT4 GetAmbientColor() const;
-	XMFLOAT4 GetDiffuseColor() const;
-
-	XMFLOAT3 GetPosition() const;
-	XMFLOAT3 GetDirection() const;
-	XMFLOAT4 GetSpecularColor() const;
-	float GetSpecularPower() const;
-
+	//~~~~ Info stored in VS Buffers ~~~~
 	void GenerateViewMatrix();
 	void GenerateProjectionMatrix(float screenDepth, float screenNear);
+	inline void SetPosition(const XMFLOAT3&pos){_position = pos;}
 
 	XMMATRIX GetViewMatrix() const;
 	XMMATRIX GetProjectionMatrix() const;
+	XMFLOAT3 GetPosition() const;
+
+	//~~~~ Info stored in PS Buffers ~~~~
+	void SetDirection(const XMFLOAT3&);
+	void SetSpecularPower(float);
+	void SetSpecularColor(const XMFLOAT4&);
+	void SetDiffuseColor(const XMFLOAT4&);
+
+	XMFLOAT3 GetDirection() const;
+	float GetSpecularPower() const;
+	XMFLOAT4 GetSpecularColor() const;
+	XMFLOAT4 GetDiffuseColor() const;
+
+	//~~~ To encapsulate later!!!!
+	void SetAmbientColor(const XMFLOAT4&);
+	XMFLOAT4 GetAmbientColor() const;
+	void SetLookAt(const XMFLOAT3&);
 
 private:
 	XMFLOAT4 _ambientColor;// @TODO - stuff into light buffer instead
